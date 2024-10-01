6b011228574e6abdc2cd2db8ab9bb4b077c94c83
zain78690@gmail.com <zain78690@gmail.com>
2024-10-01 12:44:41 +0100

    book.js done

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,31 @@
 // import the Media class:
 
 // create your Book class:
+const Media = require(`./Media`)
+
+class Book extends Media {
+    constructor(genre, year, title, author, numPages, rating) { //When passing parent through constructor, place first in the list
+        super(genre, year, title);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    // Properly formatted summary method
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    // Static method to return the book with the highest rating
+    static highestRating(books) {
+        if (books.length === 0) return null; // return null if the array is empty
+
+        return books.reduce((highest, current) => {
+            return current.rating > highest.rating ? current : highest;
+        });
+    }
+}
 
 // don't change below
 module.exports = Book;
+
